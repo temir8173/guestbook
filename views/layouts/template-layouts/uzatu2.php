00e71446bdcd38b149af18d6fce617c33784cfdb
temir8173 <temir8173@gmail.com>
2023-09-15 11:40:59 +0500

    add pixel code

--- views/layouts/template-layouts/uzatu2.php
+++ views/layouts/template-layouts/uzatu2.php
@@ -36,18 +36,35 @@ $invitation = $this->context->view->params['invitation'] ?? null;
     <meta property="og:image:width" content="300" />
     <meta property="og:image:height" content="300" />
     <?php $this->head() ?>
-    <?php 
+    <?php
         $variables = array (
             'language' => Yii::$app->language,
             // Тут обычно какие-то другие переменные
         );
         echo '<script type="text/javascript">window.my_data = ' . json_encode($variables) . ';</script>';
     ?>
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '272195845765142');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=272195845765142&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body>
 <?php $this->beginBody() ?>
 
-   
+
 <?= $content ?>
 
 <?php $this->endBody() ?>
